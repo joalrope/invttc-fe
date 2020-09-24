91abc6267d64abf17947b63f6f3d409b4a7d2cca
Jose Rodriguez <carlosjavierbermudez@MACCB.local>
2020-09-24 13:13:17 -0400

    Rutas iniciadas

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const HomePage = () => {
+export const Home = () => {
     return (
         <div>
             <h1>HomePage</h1>
--- src/routers/AuthRouter.jsx
+++ src/routers/AuthRouter.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import { LoginScreen } from '../auth/LoginScreen';
 import { RegisterScreen } from '../auth/RegisterScreen';
+import { Home } from '../components/pages/Home';
 
 export const AuthRouter = () => {
     return (
         <div className="auth__main">
             <div className="auth__box-container animate__animated animate__fadeIn">
                 <Switch >
-                    <Route path='/auth/login' component={LoginScreen} />
+                    <Route path='/' component={Home} />
                     <Route path='/auth/register' component={RegisterScreen} />
 
                     <Redirect to='/auth/login' />
