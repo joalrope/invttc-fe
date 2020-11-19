7fb79b6f075d4cd9a31bbfd8f4ec60ee4c8f724d
Jose Rodriguez <carlosjavierbermudez@MACCB.local>
2020-11-19 12:26:49 -0400

    ProductInfo II

--- src/components/sales/ProductInfo.jsx
+++ src/components/sales/ProductInfo.jsx
@@ -8,24 +8,58 @@ export const ProductInfo = () => {
     
     return (
         <div className="mt-5">
-            <h5>Detalle de Producto</h5>
             {
-                (!!activeProduct) && (
-                    <div>
-                        <p> Código: {activeProduct.code} </p>
-                        <p> Descripción: {activeProduct.title} </p>
-                        <p> Marca: {activeProduct.info[0].trademark} </p>
-                        <p> Tipo: {activeProduct.category} </p>
-                        <p> Location: {activeProduct.info[0].loc_qty[0].location} </p>
-                        <p> Cantidad: {activeProduct.info[0].loc_qty[0].qty} </p>
-                        <p> Precio Costo: {activeProduct.info[0].costPrice.toFixed(2)} </p>
-                        <p> Precio Venta: {activeProduct.info[0].salePrice.toFixed(2)} </p>
-                        <p> Medidas: {activeProduct.measurement} </p>
-                        <p> Equivalente: {activeProduct.replacement} </p>
-                    </div>
-                )
+                (!!activeProduct) && <div>
+                    <h5>Detalle de Producto</h5>
+                    <table style={{width:'100%', border: '1px solid grey', borderCollapse: 'collapse', backgroundColor: 'blueviolet'}}>
+                        <tr style={{width:'100%', border: '1px solid grey', borderCollapse: 'collapse', backgroundColor: 'grey'}}>
+                            <th>Código</th>
+                            <th>Decripción</th>
+                            <th>Categoría</th>
+                            <th>Marca</th>
+                            <th>Locación</th>
+                            <th>Cantidad</th>
+                            <th>Precio de Compra</th>
+                            <th>Precio de Venta</th>
+                            <th>Condición</th>
+                            <th>Equivalencias</th>
+                            <th>Medidas</th>
+                        </tr>
+                        <tr style={{width:'100%', border: '1px solid grey', borderCollapse: 'collapse',}}>
+                            <td rowSpan="2">{activeProduct.code}</td>
+                            <td rowSpan="2">{activeProduct.title}</td>
+                            <td rowSpan="2">{activeProduct.category}</td>
+                            <td>{activeProduct.info[0].trademark}</td>
+                            <td>{activeProduct.info[0].loc_qty[0].location} </td>
+                            <td>{activeProduct.info[0].loc_qty[0].qty} </td>
+                            <td>{activeProduct.info[0].costPrice.toFixed(2)}</td>
+                            <td>{activeProduct.info[0].salePrice.toFixed(2)}</td>
+                            <td rowSpan="2">{activeProduct.info[0].status}</td>
+                            <td rowSpan="2">{activeProduct.info[0].replacement}</td>
+                            <td rowSpan="2">{activeProduct.info[0].measurement}</td>
+                        </tr>
+                        {
+                            (!!activeProduct.info[1]) && <>
+                                <tr>
+                                    {/* <td>{activeProduct.code}</td> */}
+                                    {/* <td>{activeProduct.title}</td>
+                                    <td>{activeProduct.category}</td> */}
+                                    <td>{activeProduct.info[1].trademark}</td>
+                                    <td>{activeProduct.info[1].loc_qty[0].location} </td>
+                                    <td>{activeProduct.info[1].loc_qty[0].qty} </td>
+                                    <td>{activeProduct.info[1].costPrice.toFixed(2)}</td>
+                                    <td>{activeProduct.info[1].salePrice.toFixed(2)}</td>
+                                    {/* <td>{activeProduct.info[0].status}</td>
+                                    <td>{activeProduct.info[0].replacement}</td>
+                                    <td>{activeProduct.info[0].measurement}</td> */}
+                                </tr>
+                                
+                            </>
+                        }
+                    </table>
+                </div>
+        
             }
-            
         </div>
     )
 }
