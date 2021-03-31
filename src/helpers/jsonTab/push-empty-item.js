6e0125ad85f31e75e14faacecf628c3fa56b2bb2
joalrope <joalrope@gmail.com>
2021-03-31 16:39:33 -0400

    add objectMax & pushEmptyItem

--- src/helpers/jsonTab/push-empty-item.js
+++ src/helpers/jsonTab/push-empty-item.js
@@ -9,8 +9,8 @@ export const pushEmptyItem = (entry) => {
     result.push(item);
     
     if (item[0] > 1 && index + 1 !== entry.length) {
-        result.splice(index + addIndex + 1, 0, []);
-        addIndex++
+      result.splice(index + addIndex + 1, 0, []);
+      addIndex++
     }
   })
   return result  
