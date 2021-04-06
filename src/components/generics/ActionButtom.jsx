3a9893330232c9c5be2cc08cec4dbe5d190cd243
joalrope <joalrope@gmail.com>
2021-04-06 08:27:51 -0400

    Resolved conflict 06-04-2021

--- src/components/generics/ActionButtom.jsx
+++ src/components/generics/ActionButtom.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import '../../assets/css/action-button.scss'
 
@@ -43,48 +42,3 @@ export const ActionButtom = ({type, row, handleClick, title}) => {
     ]
   )
 }
-=======
-import React from 'react';
-import '../../assets/css/action-button.scss'
-
-export const ActionButtom = ({type, row, handleClick, title}) => {
-
-  let icon;
-
-  switch (type) {
-    case 'delete':
-      icon = 'fa fa-backspace'
-      break;
-
-    case 'edit':
-      icon = 'fa fa-edit'
-      break;
-
-    case 'select':
-      icon = 'fas fa-cart-plus'
-      break;
-
-    case 'up':
-      icon = 'fas fa-chevron-up'
-      break;
-    
-    case 'down':
-      icon = 'fas fa-chevron-down'
-      break;
-    
-    default:
-      break;
-  }
-  const rowSpan = (title) ? 3 : 1
-
-  return (
-    [<td rowSpan={rowSpan} className="td-action-button">
-      <button className= "action-button" onClick={() => handleClick(row)}>
-        <i className={icon}>{title}</i>
-      </button>
-    </td>,
-    <td></td>,
-    <td></td>]
-  )
-}
->>>>>>> a3a144f0332a50f882358e3550b1ec6314003fa2
