e7011838a2ae4621ddd954993002b232b748daf5
Jose Rodriguez <54864256+joalrope@users.noreply.github.com>
2021-05-26 08:25:00 -0400

    InventoryPage fix

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,18 +1,18 @@
-import { AboutPage } from './AboutPage';
-import { ContactPage } from './ContactPage';
-import { DashboardPage } from './DashboardPage';
-import { EquipmentPage } from './EquipmentPage';
-import { ErrorPage } from './ErrorPage';
-import { HomePage } from './HomePage';
-import { InventoryPage } from './InventoryPage';
-import { ProductsPage } from './ProductsPage';
-import { QuotePage } from './QuotePage';
-import { ReportPage } from './ReportPage';
-import { SalesPage } from './SalesPage/SalesPage';
+import { AboutPage } from "./AboutPage";
+import { ContactPage } from "./ContactPage";
+import { DashboardPage } from "./DashboardPage";
+import { EquipmentPage } from "./EquipmentPage";
+import { ErrorPage } from "./ErrorPage";
+import { HomePage } from "./HomePage";
+import { InventoryPage } from "./InventoryPage";
+import { ProductsPage } from "./ProductsPage";
+import { QuotePage } from "./QuotePage";
+import { ReportPage } from "./ReportPage";
+import { SalesPage } from "./SalesPage/SalesPage";
 
-import { ForgotPage } from './auth/ForgotPage';
-import { LoginPage } from './auth/LoginPage';
-import { RegisterPage } from './auth/RegisterPage';
+import { ForgotPage } from "./auth/ForgotPage";
+import { LoginPage } from "./auth/LoginPage";
+import { RegisterPage } from "./auth/RegisterPage";
 
 export {
   AboutPage,
